1adfca9af16d22a7007d791957c545461e45d2fe
fraangelo <angelo.francesco.mobile@gmail.com>
2014-04-29 15:55:24 +0200

    First commit

---
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/App.java
@@ -0,0 +1,13 @@
+package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
