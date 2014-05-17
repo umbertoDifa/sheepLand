abf1b7f2743e9175a1000c3288d7a535d42c8bf6
Umberto <Umberto@10.169.145.215>
2014-05-18 01:45:48 +0200

    aggiunti metodi loadFence, loadFinalFence e loadCard in classe Bank

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/Bank.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/Bank.java
@@ -22,27 +22,55 @@ public class Bank {
     public Card[] getUnusedCards() {
         return unusedCards;
     }
-    
+    /**
+     * Cerca una carta del tipo specificato nell'array delle carte disponibili
+     * e la ritorna se esiste, altrimenti solleva un eccezione se  le carte sono
+     * finite
+     * @param type Tipo di carta voluto
+     * @return una Card del tipo chiesto
+     * @throws Exception Se la carta non c'è
+     */
     public Card getCard(RegionType type) throws Exception{
-    	String errorMessage = "non ci sono più carte per il tipo"+type.toString();
-    	for( int i=type.getIndex()*GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue();
-    			i<(type.getIndex()+1)*GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue(); i++){
-    		if(this.unusedCards[i] != null){
+    	String missingCardMessage = "Non ci sono più carte per il tipo " + type.toString(); //preparo un messaggio di errore
+        //l'algoritmo che segue a come idea di indicizzare l'array
+        //le carte vengono caricate nell'array di quelle a disposizione della 
+        //banca in maniera ordinata
+    	for( int i = type.getIndex() * GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue();
+    			i < (type.getIndex() + 1) * GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue(); i++){
+    		if( this.unusedCards[i] != null ){
     			Card foundedCard = this.unusedCards[i];
     			this.unusedCards[i] = null;
     			return foundedCard;
     		}
     	}
-    	throw new Exception(errorMessage);
+    	throw new Exception(missingCardMessage);
     }
-
-    public void loadFence(){
+    /**
+     * Crea un recinto non finale e lo aggiunge all'array nella posizione 
+     * specificata
+     * @param position 
+    */
+    public void loadFence(int position){
+        unusedFences[position] = new Fence(false);
     }
-    
-    public void loadFinalFence(){
+    /**
+     * Crea un recinto finale e lo aggiunge all'array nella posizione 
+     * specificata
+     * @param position 
+     */
+    public void loadFinalFence(int position){
+        unusedFences[position] = new Fence(true);
     }
-    
-    public Fence[] getUnusedFences() {
+    /**
+     * Riceve una card e la posiziona dove richiesto nell'array delle carte
+     * non usate(del banco)
+     * @param card Carta da caricare
+     * @param position Posizione in cui caricarla
+     */
+    public void loadCard(Card card, int position){
+        unusedCards[position] = card;
+    }
+    public Fence[] getUnusedFences() { //TODO: serve?
         return unusedFences;
     }
 
