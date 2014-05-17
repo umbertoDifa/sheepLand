d9ef9f084e5bc1f7327b51d542da29748a89d16a
Umberto <Umberto@10.169.145.215>
2014-05-18 01:43:30 +0200

    aggiunti metodi setUpCards e setUpFences in GameManager, corretto setUpShepherds

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/control/GameManager.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/control/GameManager.java
@@ -1,8 +1,12 @@
 package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.control;
 
+import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Bank;
+import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Card;
+import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.GameConstants;
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Map;
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Ovine;
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Region;
+import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.RegionType;
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model.Street;
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.network.ServerThread;
 import java.util.ArrayList;
@@ -16,10 +20,12 @@ import java.util.ArrayList;
 public class GameManager {//TODO: pattern memento per ripristini?
 
     private final ServerThread server;
+
     private final Map map;
     private ArrayList<Player> players = new ArrayList<Player>();
     private final int playersNumber;
     private final int[] playersHashCode; //valore cached degli hash dei giocatori
+    private final Bank bank;
 
     public GameManager(int playersNumber, ServerThread server) {
         this.playersNumber = playersNumber;
@@ -27,7 +33,8 @@ public class GameManager {//TODO: pattern memento per ripristini?
         this.map = new Map(); //creo la mappa univoca del gioco
         this.server = server; //creo il collegamento all'univoco serverThread
         this.setUpPlayers(playersNumber); //setto arraylist giocatori e array hashcode giocatori
-
+        this.bank = new Bank(GameConstants.NUM_CARDS.getValue(),
+                GameConstants.NUM_FENCES.getValue());
     }
 
     /**
@@ -52,11 +59,12 @@ public class GameManager {//TODO: pattern memento per ripristini?
         this.setUpSocketPlayerMap();
         this.setUpAnimals();
         this.setUpShepherds();
+        this.setUpFences();
         this.setUpShift();
     }
 
     /**
-     * per ogni terreno regione della mappa aggiungi un collegamento ad un
+     * Per ogni terreno regione della mappa aggiungi un collegamento ad un
      * animale il cui tipo è scelto in maniera randomica
      */
     private void setUpAnimals() {
@@ -67,7 +75,7 @@ public class GameManager {//TODO: pattern memento per ripristini?
     }
 
     /**
-     * chiede ad ogni giocatore dove posizionare il proprio pastore
+     * Chiede ad ogni giocatore dove posizionare il proprio pastore
      */
     private void setUpShepherds() {
         String stringedStreet;
@@ -75,25 +83,29 @@ public class GameManager {//TODO: pattern memento per ripristini?
         Street chosenStreet;
         for (int i = 0; i < this.playersNumber; i++) {
             //ciclo che chiede la strada per un pastore ogni volta che questa risulta già occupata
+            //TODO: fare un metodo della roba while?
             while (true) {
                 try {
                     stringedStreet = this.server.talkTo(this.playersHashCode[i],
                             "In quale strada vuoi posizionare il pastore?"); //raccogli decisione
                     chosenStreet = convertStringToStreet(stringedStreet); //traducila in oggetto steet
-                    if ( !isStreetFree(chosenStreet) ) { //se la strada è occuapata
+                    if (!isStreetFree(chosenStreet)) { //se la strada è occuapata
                         throw new Exception(errorString); //solleva eccezione
                     }
-                    break;
+                    break;                          //altrimenti brekka il while
                 } catch (Exception e) {
                     this.server.sendTo(this.playersHashCode[i], e.getMessage());//manda il messaggio di errore al client
                 }
             }
-            //prendi il pastore dell'iesimo giocatore e spostalo sulla strada 
-            //corrispondente all'indice dell'array delle strade ottenuto
-            //convertendo la risposta ottenuta in int
-            this.players.get(i).getShepherd().moveTo(chosenStreet);
+            this.players.get(i).getShepherd().moveTo(chosenStreet); //sposta il pastore 
+            //creo una carta con valore 0 e di tipo casuale e l'aggiungo a 
+            //quelle del pastore corrispondente al mio player
+            this.players.get(i).getShepherd().addCard(new Card(0, RegionType.
+                    getRandomRegionType())); //aggiungi la carta
+            //invia conferma riepilogativa
             this.server.sendTo(this.playersHashCode[i],
-                    "Pastore posizionato");
+                    "Pastore posizionato. Hai una carta terreno di tipo: " + RegionType.MOUNTAIN.
+                    toString());
         }
 
     }
@@ -116,11 +128,60 @@ public class GameManager {//TODO: pattern memento per ripristini?
         this.server.setUpSocketPlayerMap(playersHashCode);
     }
 
+    /**
+     * Carica i recinti finali e non nel banco
+     */
+    private void setUpFences() {
+        int i; // counter
+        //salvo il numero di recinti non finali
+        int numbOfNonFinalFences = GameConstants.NUM_FENCES.getValue() - GameConstants.NUM_FINAL_FENCES.
+                getValue();
+        //carico il numero di recinti non finali in bank
+        for (i = 0; i < numbOfNonFinalFences; i++) {
+            bank.loadFence(i);
+        }
+        //carico i recinti finali in bank. 'i' parte da dove l'ha lasciato il 
+        //ciclo sopra è arriva alla fine dell'array
+        for (; i < GameConstants.NUM_FENCES.getValue(); i++) {
+            bank.loadFinalFence(i);
+        }
+    }
+
+    private void setUpCards() {
+        for (int i = 0; i < RegionType.values().length - 1; i++)            //per ogni tipo di regione - sheepsburg  
+            for (int j = 0; j < GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue(); j++) { //per tante quante sono le carte di ogni tipo
+                //crea una carta col valore giusto( j crescente da 0 al max) e tipo giusto(dipendente da i) 
+                Card cardToAdd = new Card(j, RegionType.values()[i]);
+                //calcola la posizione a cui aggiungerla(fondamentale per la getCard che
+                //usa una politica basata sugli indici)
+                int position = (i * GameConstants.NUM_CARDS_FOR_REGION_TYPE.getValue()) + j;
+                //caricala
+                bank.loadCard(cardToAdd, position);
+            }
+
+    }
+
+    /**
+     * Controlla se street è libera
+     *
+     * @param street
+     *
+     * @return true se libera, false altrimenti
+     */
     private boolean isStreetFree(Street street) {
         return street.isFree();
     }
 
+    /**
+     * converte una stringa street che indica l'id della strada sulla mappa
+     * nell'oggetto Street corrispondente
+     *
+     * @param street
+     *
+     * @return una Street
+     */
     private Street convertStringToStreet(String street) {
         return map.getStreets()[Integer.parseInt(street)];
     }
+
 }
