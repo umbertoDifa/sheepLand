24c6ef8e392654b62e69096d98578fe553de3bb5
Umberto <Umberto@10.169.145.215>
2014-05-18 03:44:54 +0200

    Aggiunte costanti a GameConstants

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/GameConstants.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/GameConstants.java
@@ -11,7 +11,9 @@ package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model
  * @author Francesco
  */
 public enum GameConstants {
-    NUM_CARDS(36), NUM_CARDS_FOR_REGION_TYPE(5), NUM_FENCES(32), NUM_REGIONS_FOR_TYPE(3), NUM_STREETS(42), NUM_REGIONS(19), NUM_FINAL_FENCES(12);
+    NUM_CARDS(36), NUM_CARDS_FOR_REGION_TYPE(5), NUM_FENCES(32), 
+    NUM_REGIONS_FOR_TYPE(3), NUM_STREETS(42), NUM_REGIONS(19), 
+    NUM_FINAL_FENCES(12), NUM_ACTIONS(3);
     private final int value;
     
 
