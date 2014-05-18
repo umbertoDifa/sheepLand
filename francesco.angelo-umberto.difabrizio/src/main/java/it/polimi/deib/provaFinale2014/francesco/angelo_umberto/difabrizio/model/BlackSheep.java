67563ba1bc5870df4c30c4d7bc2c17891fc53317
Umberto <Umberto@10.169.145.215>
2014-05-19 01:55:07 +0200

    Implementate le funzioni moveThrought della blackSheep e del Wolf

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/BlackSheep.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/BlackSheep.java
@@ -3,18 +3,23 @@ package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.model
 import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.control.CannotMoveBlackSheepException;
 
 public class BlackSheep extends SpecialAnimal {
-
+    
     @Override
     public void setAt(Region region) {
         super.setMyRegion(region);
     }
-
+    
     @Override
-    public void moveThrough(Street street) throws CannotMoveBlackSheepException {//TODO: da fare
-        //se non c'è un recinto e se esiste la strada uscita col dado
-        //e se la strada e la regione in cui la pecora sta attualmente confinano
-        //e se la regione d'arrivo confina con la strada
-
+    public void moveThrough(Street street, Region endRegion) throws
+            CannotMoveBlackSheepException {
+        //se la strada è libera 
+        if (street.isFree()) {
+            this.setAt(endRegion);//muovi la pecora nera
+        } else {
+            throw new CannotMoveBlackSheepException(
+                    "La pecora nera non può muoversi, strada occupata.");
+    
+        }
     }
-
+    
 }
--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/SpecialAnimal.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/SpecialAnimal.java
@@ -9,7 +9,7 @@ import it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.contro
 public abstract class SpecialAnimal {
     private Region myRegion;
     public void setAt(Region region){};
-    public void moveThrough(Street street) throws CannotMoveAnimalException{};
+    public void moveThrough(Street street, Region endRegion) throws CannotMoveAnimalException{};
     public Region getMyRegion() {
         return myRegion;
     }
