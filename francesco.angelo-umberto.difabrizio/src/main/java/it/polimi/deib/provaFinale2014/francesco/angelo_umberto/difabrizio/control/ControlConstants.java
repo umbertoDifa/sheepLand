d7e442eb5da39ccdd4ffe1fec877126cd282aa8a
Umberto <Umberto@10.169.149.129>
2014-05-21 02:48:23 +0200

    Aggiunte le ControlConstants

---
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/control/ControlConstants.java
@@ -0,0 +1,35 @@
+package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.control;
+
+/**
+ *
+ * @author francesco.angelo-umberto.difabrizio
+ */
+public enum ControlConstants {
+
+    /**
+     * Indica quanti pastori hanno i giocatori nel caso di un gioco con pochi
+     * giocatori
+     */
+    SHEPHERD_FOR_FEW_PLAYERS(2),
+    /**
+     * Indica da quanti giocatori è composto una partita piccola, nel quale ogni
+     * player ha diritto a SHEPHERD_FOR_FEW_PLAYERS pastori
+     */
+    NUM_FEW_PLAYERS(2),
+    /**
+     * Indica quanti pastori ha ogni giocatore durante un gioco normale
+     */
+    STANDARD_SHEPHERD_FOR_PLAYER(1);
+    private final int value;
+
+    ControlConstants(int value) {
+        this.value = value;
+    }
+
+    /**
+     * @return Valore corrispondente alla costante in questione
+     */
+    public int getValue() {
+        return this.value;
+    }
+}
