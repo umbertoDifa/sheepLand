ff67446dec35ce9f3f21d4be075978649b2109ac
fraangelo <angelo.francesco.mobile@gmail.com>
2014-05-22 13:32:14 +0200

    formattati commenti Dice

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/Dice.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/Dice.java
@@ -4,25 +4,28 @@ import java.util.Random;
 
 /**
  * Il dado
+ *
  * @author francesco.angelo-umberto.difabrizio
  */
 public class Dice {
 
     private static final int DICE_FACES = 6;
     private static final Random dice = new Random();
-    
-    /**
-     * Lancia il dado e torna un valore random tra 1 e 6
-     * @return Il valore che esce dal lancio del dado
-     */
-    public static int getRandomValue() {
-        //nextInt mi ritorna un numero tra 0 e 5, aggiungo 1 cosi diventa tra 1 e 6                                             
-        return dice.nextInt(DICE_FACES) + 1;
-    }
-    
+
     /**
      * Metto il costruttore privato per non permettere di istanziarlo
      */
-    private Dice(){
+    private Dice() {
     }
+
+    /**
+     * Lancia il dado e torna un valore random tra 1 e 6
+     *
+     * @return Il valore che esce dal lancio del dado
+     */
+    public static int roll() {
+        //nextInt mi ritorna un numero tra 0 e 5, aggiungo 1 cosi diventa tra 1 e 6                                             
+        return dice.nextInt(DICE_FACES) + 1;
+    }
+
 }
