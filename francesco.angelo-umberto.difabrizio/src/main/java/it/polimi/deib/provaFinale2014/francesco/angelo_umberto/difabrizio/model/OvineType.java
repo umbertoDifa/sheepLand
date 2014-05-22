5521a277a01972feefc744345bd22eb538e03e47
fraangelo <angelo.francesco.mobile@gmail.com>
2014-05-22 13:08:22 +0200

    formattati commenti in OvineType

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/OvineType.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/model/OvineType.java
@@ -4,17 +4,19 @@ import java.util.Random;
 
 /**
  * Tutti i tipi di ovini possibili.
+ *
  * @author Francesco
  */
 public enum OvineType {
 
     SHEEP, RAM, LAMB;
+    //size dell'enum cached così non la ricalco ogni volta
+    private static final int size = OvineType.values().length;
+    //oggetto random cached
+    private static final Random random = new Random();
 
-    private static final int size = OvineType.values().length; //size dell'enum cached così non la ricalco ogni volta
-    private static final Random random = new Random(); //oggetto random cached
-    
     /**
-     * 
+     *
      * @return Ritorna il tipo default di ovino(pecora)
      */
     static OvineType getDefaultOvineType() {
@@ -26,7 +28,9 @@ public enum OvineType {
      * @return OvineType Un tipo a caso di ovino nella enum
      */
     static OvineType getRandomOvineType() {
-        int choice = random.nextInt(size); //prendi un numero a caso appartenente al totale dei valori dell'enum
-        return OvineType.values()[choice]; //ritorno l'ovino corrispondente
+        //prendi un numero a caso appartenente al totale dei valori dell'enum
+        int choice = random.nextInt(size);
+        //ritorno l'ovino corrispondente
+        return OvineType.values()[choice];
     }
 }
