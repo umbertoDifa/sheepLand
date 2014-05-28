051a96200368af09dadbeb0c9d829c0cdb5ea190
Umberto <Umberto@10.169.146.59>
2014-05-28 18:13:16 +0200

    interfacce rmi socket client

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/network/RemoteClientRmi.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/network/RemoteClientRmi.java
@@ -7,65 +7,46 @@ import java.util.List;
  *
  * @author francesco.angelo-umberto.difabrizio
  */
-public class RemoteClientRmi implements Remote {
+public interface RemoteClientRmi extends Remote {
 
     public void refreshRegion(int regionIndex, int numbOfSheep, int numbOfRam,
-                              int numbOfLamb) {
-    }
+                              int numbOfLamb);
 
     public void refreshStreet(int streetIndex, boolean Fence,
-                              String nickShepherd) {
-    }
+                              String nickShepherd);
 
     public void refereshGameParameters(int numbOfPlayers, String firstPlayer,
-                                       int shepherd4player) {
-    }
+                                       int shepherd4player);
 
-    public void refereshCurrentPlayer(String currenPlayer) {
-    }
+    public void refereshCurrentPlayer(String currenPlayer);
 
-    public void refereshCards(List<String> myCards) {
-    }
+    public void refereshCards(List<String> myCards);
 
-    public void refreshBlackSheep(int regionIndex) {
-    }
+    public void refreshBlackSheep(int regionIndex);
 
-    public void refreshWolf(int regionIndex) {
-    }
+    public void refreshWolf(int regionIndex);
 
-    public void setUpShepherds() {
-    }
+    public void setUpShepherds();
 
-    public void chooseAction() {
-    }
+    public void chooseAction();
 
-    public void moveOvine() {
-    }
+    public void moveOvine();
 
     public void refreshMoveOvine(int startRegionIndex, int endRegionIndex,
-                                 String type) {
+                                 String type);
 
-    }
+    public void moveShepherd();
 
-    public void moveShepherd() {
-    }
+    public void refreshMoveShepherd();
 
-    public void refreshMoveShepherd() {
-    }
+    public void buyLand();
 
-    public void buyLand() {
-    }
+    public void mateSheepWith();
 
-    public void mateSheepWith() {
-    }
+    public void refreshMateSheepWith(int regionIndex, String ovineType);
 
-    public void refreshMateSheepWith(int regionIndex, String ovineType) {
-    }
+    public void killOvine();
 
-    public void killOvine() {
-    }
-
-    public void refreshKillOvine(int regionIndex) {
-    }
+    public void refreshKillOvine(int regionIndex);
 
 }
