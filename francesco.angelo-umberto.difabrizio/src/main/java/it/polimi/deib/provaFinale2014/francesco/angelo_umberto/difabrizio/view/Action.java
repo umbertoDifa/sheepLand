771e680b1f9cd95a85943817adba1580b7cd82d2
fraangelo <angelo.francesco.mobile@gmail.com>
2014-06-12 12:04:21 +0200

    aggiunta doc action

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Action.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Action.java
@@ -7,12 +7,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 /**
- *
+ * The class Action extends BackgroundAndTextJPanel. It adds the possibility
+ * to set the opacity and a tooltipText.
  * @author Francesco
  */
 public class Action extends BackgroundAndTextJPanel implements MouseListener {
     private final String toolTipText;
     
+    /**
+     * Create a JPanel Action, with the toolTipText indicated.
+     * It removes the background color, add itself to the mouselistener
+     * for cursor effects.  It switch on the opacity 
+     * @param toolTipText 
+     */
     public Action(String toolTipText) {
         setOpacity(true);
         this.setBackground(new Color(0, 0, 0, 0));
@@ -22,14 +29,17 @@ public class Action extends BackgroundAndTextJPanel implements MouseListener {
     }
 
     /**
-     * se ok l'img viene visualizzata non opaca
-     *
+     * iff ok true, the image is opaque
      * @param ok
      */
     protected void setOpaqueView(boolean ok) {
         super.setOpacity(ok);
     }
 
+    /**
+     * Debug method
+     * @param e 
+     */
     public void mouseClicked(MouseEvent e) {
         DebugLogger.println("azione clickata, dentro la catch dell evento");
         revalidate();
@@ -42,10 +52,18 @@ public class Action extends BackgroundAndTextJPanel implements MouseListener {
     public void mouseReleased(MouseEvent e) {
     }
 
+    /**
+     * When the mouse enters, the cursor becames Hand
+     * @param e 
+     */
     public void mouseEntered(MouseEvent e) {
         this.setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
+    /**
+     * when the mouse exits the cursor return default cursor
+     * @param e 
+     */
     public void mouseExited(MouseEvent e) {
         this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
