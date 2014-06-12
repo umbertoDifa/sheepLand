8e26553613d45395ec655dd8a11e3f750c1d0583
fraangelo <angelo.francesco.mobile@gmail.com>
2014-06-12 12:05:06 +0200

    aggiunta doc card

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Card.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Card.java
@@ -9,6 +9,8 @@ import java.awt.event.MouseListener;
 import javax.swing.JLabel;
 
 /**
+ * The class extends CardBoard. It adds a jlabel for the bank current value of
+ * the card.
  *
  * @author Francesco
  */
@@ -16,6 +18,15 @@ public class Card extends CardBoard implements MouseListener {
 
     JLabel bankNum;
 
+    /**
+     * create the Card with inside a JLabel with the indicated font, text for
+     * the bank value and number for the occurrence of the card. Add itself as
+     * MouseListener for cursor effect
+     *
+     * @param font
+     * @param bankNum
+     * @param myCardNum
+     */
     public Card(Font font, String bankNum, String myCardNum) {
         super(font, myCardNum);
         this.bankNum = new JLabel(bankNum);
@@ -27,6 +38,11 @@ public class Card extends CardBoard implements MouseListener {
         this.addMouseListener(this);
     }
 
+    /**
+     * set the bank value of the card
+     *
+     * @param amountToAdd
+     */
     public void increase(int amountToAdd) {
         //calcolo nuovo ammount
         int newAmmount = Integer.parseInt(this.getText()) + amountToAdd;
@@ -34,15 +50,25 @@ public class Card extends CardBoard implements MouseListener {
         //setto il nuovo ammount
         this.setText(Integer.toString(newAmmount));
     }
-    
+
+    /**
+     * se text of the card
+     *
+     * @param text
+     */
     @Override
     public void setText(String text) {
-        if(Integer.parseInt(text)<0){
+        if (Integer.parseInt(text) < 0) {
             this.setUp(".\\images\\numFencesRed.png", 67, 77, 78, 94);
         }
         super.setText(text);
     }
 
+    /**
+     * debug method
+     *
+     * @param e
+     */
     public void mouseClicked(MouseEvent e) {
         DebugLogger.println("carta terreno clickata, dentro la catch dell evento");
         repaint();
@@ -56,10 +82,20 @@ public class Card extends CardBoard implements MouseListener {
 
     }
 
+    /**
+     * When the mouse enters, the cursor becames Hand
+     *
+     * @param e
+     */
     public void mouseEntered(MouseEvent e) {
         this.setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
 
+    /**
+     * when the mouse exits the cursor return default cursor
+     *
+     * @param e
+     */
     public void mouseExited(MouseEvent e) {
         this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
