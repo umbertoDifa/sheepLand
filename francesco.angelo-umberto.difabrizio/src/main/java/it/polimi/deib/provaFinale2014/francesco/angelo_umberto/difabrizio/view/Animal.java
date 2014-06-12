2f0431a72a331c1faf3215403fd453d66c588900
fraangelo <angelo.francesco.mobile@gmail.com>
2014-06-12 12:04:51 +0200

    aggiunta doc animal

--- francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Animal.java
+++ francesco.angelo-umberto.difabrizio/src/main/java/it/polimi/deib/provaFinale2014/francesco/angelo_umberto/difabrizio/view/Animal.java
@@ -1,18 +1,17 @@
 package it.polimi.deib.provaFinale2014.francesco.angelo_umberto.difabrizio.view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.TextArea;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
- *
+ * The class Animal can be used in 2 way, preview
+ * and default. The class has a JLabel for indicate
+ * the number of the animal, except for animaltype
+ * "blacksheep" and "wolf"
  * @author Francesco
  */
 public class Animal extends JPanel {
@@ -27,6 +26,14 @@ public class Animal extends JPanel {
     private final String animalType;
     private JLabel numLabel;
 
+    /**
+     * creates the Animal of the indicated type.
+     * sets 2 images base on the type: preview and default image.
+     * it sets the dimension for both. sets preview to true.
+     * if the animal is not blacksheep and wolf set the jlabel
+     * initializing it to "1"
+     * @param animalType 
+     */
     public Animal(String animalType) {
         this.animalType = animalType;
         imagePreview = ImagePool.getByName(animalType + "P");
@@ -47,10 +54,18 @@ public class Animal extends JPanel {
         }
     }
 
+    /**
+     * returns the animal type
+     * @return 
+     */
     public String getAnimalType() {
         return animalType;
     }
 
+    /**
+     * Refresh the bounds of the animal according to
+     * the modality of view
+     */
     protected void refreshLabelBounds() {
         numLabel.setSize(28, 28);
         if (preview) {
@@ -63,6 +78,10 @@ public class Animal extends JPanel {
         }
     }
 
+    /**
+     * set, if exists, the background image
+     * @param g 
+     */
     @Override
     protected void paintComponent(Graphics g) {
         setOpaque(false);
@@ -77,6 +96,10 @@ public class Animal extends JPanel {
         super.paintComponent(g);
     }
 
+    /**
+     * return the dimensions according to the modality of view
+     * @return 
+     */
     @Override
     public Dimension getPreferredSize() {
         if (preview) {
@@ -86,6 +109,10 @@ public class Animal extends JPanel {
         }
     }
 
+    /**
+     *  return the dimensions according to the modality of view
+     * @return 
+     */
     @Override
     public Dimension getSize() {
         if (preview) {
@@ -95,6 +122,11 @@ public class Animal extends JPanel {
         }
     }
 
+    /**
+     * set the modality of view. iff ok is true
+     * the modality will be "preview"
+     * @param ok 
+     */
     public void setPreview(boolean ok) {
         preview = ok;
         if (this.numLabel != null) {
@@ -103,6 +135,10 @@ public class Animal extends JPanel {
         repaint();
     }
 
+    /**
+     * return the number of the label, if exists
+     * @return 
+     */
     public int getNum() {
         if (numLabel != null) {
             return Integer.parseInt(numLabel.getText());
@@ -110,12 +146,22 @@ public class Animal extends JPanel {
         return 0;
     }
 
+    /**
+     * set, if exists, the number og the label
+     * @param num 
+     */
     public void setNum(int num) {
         if (numLabel != null) {
             this.numLabel.setText(String.valueOf(num));
         }
     }
 
+    /**
+     * create a clone of the animal, with same preview modality
+     * and label, if exists
+     * @return
+     * @throws CloneNotSupportedException 
+     */
     @Override
     protected Animal clone() throws CloneNotSupportedException {
         Animal newAnimal = new Animal(this.animalType);
